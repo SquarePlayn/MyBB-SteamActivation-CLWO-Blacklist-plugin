b9ee258aba858fe30b12f9c82d09a82a744517c9
Square Playn <SquarePlayn@users.noreply.github.com>
2017-04-13 21:22:33 +0200

    Update V1.1
    Changelog:
    Plugin now uses the steam_activation setting to switch between (not) using CURL
    Fixed bug where non-permanent banned people got banned for 1/60 the time, 60 times.
    Changed: Users now get banned for the time they have left on their blacklist instead of the full time the initially were banned for
    Added: Users who were forum-banned for being blacklisted now get automatically unbanned when unblacklisted

--- steam_activation_customcheck_blacklist.php
+++ steam_activation_customcheck_blacklist.php
@@ -7,31 +7,31 @@ if(!defined("IN_MYBB")) {
 
 function steam_activation_customcheck_blacklist($forumUserID, $steamID) {
 	steam_activation_f_debug("Function: customcheck_blacklist()");
+	global $mybb, $db;
 
 	if(isset($steamID)) {
 		steam_activation_f_debug("Steam ID found: ".$steamID);
 
 		$apilink = "https://clwo.eu/jailbreak/api/v2/blacklist.php?cSteamID64=".$steamID;
 
-		/* OLD PHP FILE_GET_CONTENTS WAY THAT DOES NOT USE CURL
-		** FEEL FREE TO UNCOMMENT THIS OLD WAY AND COMMENT OUT CURL IF YOU PREFER SO
-		*/ 
-		//$content = file_get_contents($apilink);
-
-		/* NEW CURL WAY */
-		$curler = curl_init();
-		curl_setopt($curler, CURLOPT_SSL_VERIFYPEER, false);
-		curl_setopt($curler, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($curler, CURLOPT_URL, $apilink);
-		$content = curl_exec($curler);
-		curl_close($curler);
-		/* END OF CURL*/
+		if($mybb->settings["steam_activation_curl"]) {
+			steam_activation_f_debug("Using CURL to get blacklist info");
+			$curler = curl_init();
+			curl_setopt($curler, CURLOPT_SSL_VERIFYPEER, false);
+			curl_setopt($curler, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($curler, CURLOPT_URL, $apilink);
+			$content = curl_exec($curler);
+			curl_close($curler);
+		} else {
+			steam_activation_f_debug("NOT using CURL to get blacklist info, but doing it the non-curl way");
+			$content = file_get_contents($apilink);
+		}
 
 		$blacklistinfo = json_decode($content, true);
 
 		if($blacklistinfo["status"] == 200) {
 			//API Online
-			steam_activation_f_debug("API online");
+			steam_activation_f_debug("Blacklist API online");
 
 			steam_activation_f_debug("Results: ".$blacklistinfo["results"]);
 
@@ -43,7 +43,7 @@ function steam_activation_customcheck_blacklist($forumUserID, $steamID) {
 
 				$accountID = $baninfo["cAccountID"];
 				$blacklistreason = $baninfo["Reason"];
-				$banlength = $baninfo["ExpiresInMinutes"];
+				$banlength = $baninfo["ExpiresInMinutes"] * 60;
 				$link = "https://clwo.eu/jailbreak/admin/view-blacklist.php?AccountID=".$accountID;
 
 				if($baninfo["Perm"] == 1) {
@@ -52,6 +52,9 @@ function steam_activation_customcheck_blacklist($forumUserID, $steamID) {
 					$banlength = -1;
 				}
 
+				steam_activation_f_debug("Lenght: ".$banlength);
+
+				//NB, WHEN CHANGING THE REASON FORMAT, CHECK THE UNBAN PROCEDURE THAT IS BELOW
 				$returnarray = array(
 					"error" => "blacklisted",
 					"banreason" => "You are blacklisted. Check ".$link,
@@ -64,6 +67,21 @@ function steam_activation_customcheck_blacklist($forumUserID, $steamID) {
 			} else {
 				//Not blacklisted
 				steam_activation_f_debug("Not blacklisted");
+
+				steam_activation_f_debug("Going to check if this forum user was banned for blacklist.");
+				$forumbaninfo = $db->simple_select("banned", "reason", "uid=".$forumUserID);
+				if($db->num_rows($forumbaninfo)){
+				    steam_activation_f_debug("The user is banned for something. Let's see if it is a blacklist.");
+				    $banreason = $db->fetch_array($forumbaninfo)["reason"];
+				    if(explode(".",$banreason)[0] === "You are blacklisted"){
+				        steam_activation_f_debug("User was previously banned for being blacklisted. Unbanning.");
+				        return array("error" => "unban");
+                    } else {
+				        steam_activation_f_debug("User was banned for a reason other than a blakclist. Cya!");
+                    }
+                } else {
+				    steam_activation_f_debug("The user is not banned off the forums. Cya!");
+                }
 
 				//Returning empty array since no problem found
 				return array();
